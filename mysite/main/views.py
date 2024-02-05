5abe948fb9b49df6eb0dff959ba101f8af92f690
zilsoft <zilsoft@proton.me>
2024-02-05 17:37:11 +0300

    First templates and css

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -3,16 +3,16 @@ from django.shortcuts import render
 
 
 def index(request):
-    context = {
-        "title": "Главная",
-        "content": "Главная страница магазина STORE",
-        "list": ["first", "second"],
-        "dict": {"first": 1},
-        "bool": False,
-    }
+    context = {"title": "Home - Главная", "content": "Магазин мебели HOME"}
 
-    return render(request, "index.html", context)
+    return render(request, "main/index.html", context)
 
 
 def about(request):
-    return HttpResponse("О нас")
+    context = {
+        "title": "Home - О нас",
+        "content": "О нас",
+        "text_on_page": "Текст о том почему этот магазин такой классный, и какой хороший товар.",
+    }
+
+    return render(request, "main/about.html", context)
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,7 +20,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("", views.index, name="index"),
     path("about/", views.about, name="about"),
 ]
